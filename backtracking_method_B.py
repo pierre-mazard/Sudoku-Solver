692f6f8d5dae6d1ba36c45ed2cda75103779d29a
isabellek-source <129529988+isabellek-source@users.noreply.github.com>
2024-03-26 15:47:33 +0100

    Remove .DS_Store from everywhere

--- backtracking_method_B.py
+++ backtracking_method_B.py
@@ -1,18 +1,14 @@
-# Code with object-oriented programming
-#Sudoku text files
-sudoku1="Sudoku-Board/sudoku1.txt"
-sudoku2="Sudoku-Board/sudoku2.txt"
-sudoku3="Sudoku-Board/sudoku3.txt"
-sudoku4="Sudoku-Board/sudoku4.txt"
-sudoku5="Sudoku-Board/sudoku5.txt"
-
 # Creating my class SudokuSolver 
 class SudokuSolver():
-    # Initializing the class SudokuSolver : incorporating the file_name as the object to be processed
-    def __init__(self, file_name):
-        self.grid = self.read_file(file_name)
-        #self.grid will be used as the principal object
-    
+    # Initializing the class SudokuSolver with the possibility to choose between 5 files
+    def __init__(self):
+        self.sudoku_txt = ["Sudoku-Board/sudoku1.txt",
+                           "Sudoku-Board/sudoku2.txt",
+                           "Sudoku-Board/sudoku3.txt",
+                           "Sudoku-Board/sudoku4.txt",
+                           "Sudoku-Board/sudoku5.txt"]
+        self.grid = None
+
     # Function to specify how to read the file
     def read_file(self, name_file):
         with open(name_file, 'r') as reading:
@@ -40,7 +36,6 @@ class SudokuSolver():
                 for j in range(3):
                     if self.grid[start_row + i][start_col + j] == num:
                         return False
-                    
             return True
 
         # Function to solve sudoku with numbers to fill 
@@ -58,7 +53,7 @@ class SudokuSolver():
             # if row and line are full, go to the next line
             if self.grid[row][col] != 0:
                 return solve_next_empty(row, col + 1)
-            
+
             # Loop to introduce numbers from 1 to 9 at the empty spots 
             for num in range(1, 10):
                 if is_valid(num, row, col):
@@ -67,32 +62,40 @@ class SudokuSolver():
                     # Solve the sudoku by replacing the empty spot with the exact number
                     if solve_next_empty(row, col + 1):
                         return True
-                    
+
                     # If it's not valide, back to 0 
                     self.grid[row][col] = 0
 
             return False
-
         return solve_next_empty(0, 0)
-
 
     # Visualization of the sudoku grid 
     def print_grid(self):
         for row in self.grid:
             print(row)
+    
+    def solve_selected_sudoku(self, file_index):
+        if 1 <= file_index <= len(self.sudoku_txt):
+            self.file_name = self.sudoku_txt[file_index - 1]
+            self.grid = self.read_file(self.file_name)
 
-    # Function to show the solved sudoku
-    def show_solved_sudoku(self):
+            if self.solve_sudoku():
+                print(f"""⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒
+          Solution :
+⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒""")
+                self.print_grid()
+                print(f"""⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒
+Bravo! ton sudoku a été résolu!
+⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒""")
+            else: 
+                print("Désolée, je n'ai pas de solution.")
+        else: 
+            print("Erreur : veuillez choisir un nombre entre 1 et 5.")
 
-    #  Excecution of my solve function and printing the result
-        if self.solve_sudoku():
-            print("Solution: \n ")
-            self.print_grid()
-            print("Congratulation, your sudoku is solved")
-
-        else:
-            print("No solution.")
-
+    def print_sudoku_files(self):
+        print("Veuillez choisir un fichier (entre 1 et 5) : \n")
+        for i, file_name in enumerate (self.sudoku_txt, start=1):
+            print(f"{i}. {file_name}")
 
 # Once all the code is run, test with cProfile.run() to have the total run time taken
 # by the entire code
